5af052d687184fe1561ff8e049b2e700b093744c
Pedro Tunin <pedrotunin@gmail.com>
2021-07-07 11:21:08 -0300

    Aula 6 adicionada.

--- aula6/relatorio/ImpressaoSimples.java
+++ aula6/relatorio/ImpressaoSimples.java
@@ -4,8 +4,7 @@ public class ImpressaoSimples implements PluginImpressao {
 
     @Override
     public void imprimir(Relatorio r) {
-        int a = 0;
-        //Impressao.imprimirLinha(r.getTexto());
+        Impressao.imprimirLinha(r.getTexto());
     }
     
 }
